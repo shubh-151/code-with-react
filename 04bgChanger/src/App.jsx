e76ab04c3ebd8cc8cd6450c6ce9fe7099202ab9f
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-10-08 01:57:31 +0530

    bg changer updated

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,13 +1,54 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [color, setColor] = useState("olive")
+  const [color, setColor] = useState("olive");
 
   return (
-    <div>
-      
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="fixed flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor("Red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-sm"
+            style={{ backgroundColor: "Red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("Green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-sm"
+            style={{ backgroundColor: "Green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("Gray")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-sm"
+            style={{ backgroundColor: "Gray" }}
+          >
+            Gray
+          </button>
+          <button
+            onClick={() => setColor("Blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-sm"
+            style={{ backgroundColor: "Blue" }}
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => setColor("Yellow")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-sm"
+            style={{ backgroundColor: "Yellow" }}
+          >
+            Yellow
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
