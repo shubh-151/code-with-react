612fb8b907bc4b8bcbb655ec18e74ebbc5d8a684
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-10-12 23:02:16 +0530

    React Router

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
-
+import React, { useState,useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
 const Github = () => {
-    const [data, setData] = useState([])
-useEffect(()=>{
-    fetch('https://api.github.com/users/shubh-151')
-    .then(response => response.json())
-    .then(data=>{
-        console.log(data)
-        setData(data)
-    })
-})
+
+   const data = useLoaderData()
+//     const [data, setData] = useState([])
+// useEffect(()=>{
+//     fetch('https://api.github.com/users/shubh-151')
+//     .then(response => response.json())
+//     .then(data=>{
+//         console.log(data)
+//         setData(data)
+//     })
+// })
 
   return (
     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
@@ -21,3 +22,9 @@ useEffect(()=>{
 };
 
 export default Github;
+
+
+export const githubInfoLoader = async ()=>{
+    const response = await fetch ('https://api.github.com/users/shubh-151')
+    return response.json()     
+}
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -13,7 +13,7 @@ import Layout from "./components/Layout.jsx";
 import About from "./components/About/About.jsx";
 import Contact from "./components/Contact/Contact.jsx";
 import User from "./components/User/User.jsx";
-import Github from "./components/Github/Github.jsx";
+import Github, { githubInfoLoader } from "./components/Github/Github.jsx";
 
 // const router = createBrowserRouter([
 //   {
@@ -42,7 +42,10 @@ const router = createBrowserRouter(
     <Route path='about' element={<About/>}/>
     <Route path='contact' element={<Contact/>}/>
     <Route path='user/:userid' element={<User/>}/>
-    <Route path='github' element={<Github/>}/>
+    <Route 
+    loader={githubInfoLoader}
+    path='github' 
+    element={<Github/>}/>
   </Route>),
 );
 
