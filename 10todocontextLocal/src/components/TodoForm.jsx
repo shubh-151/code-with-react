766635b510f9bda6fc2ad204f5dd051106d91b45
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-10-15 02:03:14 +0530

    dgsr

--- 10todocontextLocal/src/components/TodoForm.jsx
+++ 10todocontextLocal/src/components/TodoForm.jsx
@@ -15,6 +15,7 @@ function TodoForm() {
     <form onSubmit={add} className="flex">
       <input
         value={todo}
+        onChange={(e) =>setTodo(e.target.value)}
         type="text"
         placeholder="Write Todo..."
         className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
