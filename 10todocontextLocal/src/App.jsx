3e736bd97dcbe6f4cbad5a39c8cf13a6ca5c67a8
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-10-15 02:48:11 +0530

    To Do using local storage

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { TodoProvider } from "./contexts";
 import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
 function App() {
   const [todos, setTodos] = useState([]);
   
@@ -17,7 +19,9 @@ const deleteTodo = (id) =>{
 }
 
 const toggleComplete = (id) =>{
-  setTodos((prev) => prev.map((prevTodo) => prevTodo===id ? {...prevTodo, completed:!prevTodo.completed} : prevTodo))
+  setTodos((prev) => 
+  prev.map((prevTodo) => 
+  prevTodo.id === id ? {...prevTodo, completed:!prevTodo.completed} : prevTodo))
 }
 
 useEffect(() =>{
@@ -44,6 +48,14 @@ useEffect(() =>{
           </div>
         <div className="flex flex-wrap gap-y-3">
           {/*Loop and Add TodoItem here */}
+          {todos.map((todo) => (
+            <div key={todo.id}
+            className="w-full"
+            >
+              <TodoItem todo={todo}/> 
+
+            </div>
+          ))}
         </div>
       </div>
     </div>
