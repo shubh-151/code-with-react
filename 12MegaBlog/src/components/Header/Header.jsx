cce2d7569ef24e8e5b3dbc1e1117254ad8fc96a8
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-10-24 19:53:44 +0530

    nihj

--- 12MegaBlog/src/components/Header/Header.jsx
+++ 12MegaBlog/src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Container, Logo, LogoutBtn } from "../index";
-import { UseSelector, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const authStatus = useSelector((state) => state.auth.status);
   const navigate = useNavigate();
 
-  const navItem = [
+  const navItems = [
     {
       name: "Home",
       slug: "/",
@@ -35,9 +35,36 @@ const Header = () => {
     },
   ];
   return (
-    <div>
-      <h1>header</h1>
-    </div>
+    <header>
+      <Container>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo width="70px" />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
   );
 };
 
