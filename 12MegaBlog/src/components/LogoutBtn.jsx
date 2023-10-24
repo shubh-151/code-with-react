810d61b2df469123fec63681e7ac5f068729f277
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-10-24 15:52:20 +0530

    inlkjlk

---
+++ 12MegaBlog/src/components/LogoutBtn.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {useDispatch} from 'react-redux'
+import authServicenfrom 
+const LogoutBtn = () => {
+  return (
+    <div>
+
+    <auth
+      logout
+    </div>
+  )
+}
+
+export default LogoutBtn
