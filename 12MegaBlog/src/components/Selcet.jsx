c8568adb40f85fa0206a9006da6bf36a8ab15852
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-10-26 12:23:16 +0530

    leinrcgej

---
+++ 12MegaBlog/src/components/Selcet.jsx
@@ -0,0 +1,18 @@
+import React, { useId } from 'react'
+
+function Selcet({
+    option,
+    label,
+    className,
+    ...props
+},ref) {
+
+    const id = useId()
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Selcet
