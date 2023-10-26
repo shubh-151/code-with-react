4388188c0a2b1c9c1a693d9414622265ddec84cc
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-10-26 12:38:15 +0530

    vmodr

--- 12MegaBlog/src/components/Selcet.jsx
+++ 12MegaBlog/src/components/Selcet.jsx
@@ -1,18 +1,20 @@
-import React, { useId } from 'react'
+import React, { useId } from "react";
 
-function Selcet({
-    option,
-    label,
-    className,
-    ...props
-},ref) {
-
-    const id = useId()
+function Selcet({ option, label, className, ...props }, ref) {
+  const id = useId();
   return (
-    <div>
-      
+    <div className="w-full">
+      {label && <label htmlFor="{id}"></label>}
+      <select
+        {...props}
+        id={id}
+        ref={ref}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+      >
+        
+      </select>
     </div>
-  )
+  );
 }
 
-export default Selcet
+export default Selcet;
