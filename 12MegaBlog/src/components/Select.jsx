75bd39440335ffb1ba4dea41cd615c996142236d
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-10-26 12:45:57 +0530

    ecgsef

--- 12MegaBlog/src/components/Select.jsx
+++ 12MegaBlog/src/components/Select.jsx
@@ -1,6 +1,6 @@
 import React, { useId } from "react";
 
-function Selcet({ option, label, className, ...props }, ref) {
+function Select({ option, label, className, ...props }, ref) {
   const id = useId();
   return (
     <div className="w-full">
@@ -11,10 +11,14 @@ function Selcet({ option, label, className, ...props }, ref) {
         ref={ref}
         className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
       >
-        
+        {options?.map((option)=>(
+            <option value="">
+                {option key = {option} value ={option}}
+            </option>
+        ))}
       </select>
     </div>
   );
 }
 
-export default Selcet;
+export default Select;
