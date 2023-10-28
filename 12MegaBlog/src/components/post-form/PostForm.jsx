9cd45c68c346f8b17a28899e7a37b3af006f2c94
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-10-28 20:48:34 +0530

    .oomijlimj

---
+++ 12MegaBlog/src/components/post-form/PostForm.jsx
@@ -0,0 +1,14 @@
+import React,{useCallback} from 'react'
+import {useFrom} from 'react-hook-form'
+import {Button, Input, Select, RTE } from '../index'
+
+
+const PostForm = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PostForm
