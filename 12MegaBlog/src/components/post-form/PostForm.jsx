92252d9e944146be317bd7203b1d7125b15b8330
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-10-28 21:28:26 +0530

    ;omij;k

--- 12MegaBlog/src/components/post-form/PostForm.jsx
+++ 12MegaBlog/src/components/post-form/PostForm.jsx
@@ -1,9 +1,29 @@
 import React,{useCallback} from 'react'
-import {useFrom} from 'react-hook-form'
+import {useForm} from 'react-hook-form'
 import {Button, Input, Select, RTE } from '../index'
+import  AppwriteService  from '../../appwrite/config'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 
-const PostForm = () => {
+const PostForm = ({post}) => {
+    const {register, handleSubmit, watch, setValue, control, getValues} = useForm({
+        defaultValues:{
+            title: post?.title || '',
+            slug:post?.slug || '',
+            content:post?.content || '',
+            status:post?.status || 'active',
+        },
+    })
+
+    const navigate = useNavigate()
+    const userData = useSelector(state => state.user.userData )
+const submit  = async (data) =>{
+    if (post){
+        const file = data.image[0]?AppwriteService.uploadFile(data.image[0]):null
+    }
+}
+
   return (
     <div>
       
