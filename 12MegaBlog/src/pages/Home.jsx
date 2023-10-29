0e66bfc90e3676b5e7ac3ddc51dc63ba8d0bf6d7
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-10-29 22:46:09 +0530

    dfdgfdf

--- 12MegaBlog/src/pages/Home.jsx
+++ 12MegaBlog/src/pages/Home.jsx
@@ -9,8 +9,24 @@ const Home = () => {
       if (posts) {
         setPosts(posts.documents);
       }
-    }, []);
-  });
+    });
+  }, []);
+
+  if (posts.length === 0) {
+    return (
+      <div className="w-full py-8">
+        <Container>
+          <div className="flex flex-wrap">
+            {posts.map((post) => (
+              <div key={post.$id} className="p-2 w-1/4">
+                <PostCard {...post} />
+              </div>
+            ))}
+          </div>
+        </Container>
+      </div>
+    );
+  }
 
   return <div></div>;
 };
